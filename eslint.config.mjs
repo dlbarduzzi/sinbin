76cc8b1b1d3158443dd479bae348f165f3fa9c4c
Dylan Barduzzi <dlbarduzzi@gmail.com>
2025-03-26 14:14:59 -0400

    Setup eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,43 @@
+import antfu from "@antfu/eslint-config"
+
+export default antfu({
+  type: "app",
+  typescript: true,
+  formatters: true,
+  stylistic: {
+    indent: 2,
+    semi: false,
+    quotes: "double",
+  },
+  ignores: ["node_modules/**"],
+}, {
+  rules: {
+    "style/arrow-parens": "off",
+    "no-empty": "error",
+    "no-empty-function": "error",
+    "node/no-process-env": "error",
+    "node/no-process-exit": "error",
+    "node/prefer-global/process": "off",
+    "prefer-const": "error",
+    "perfectionist/sort-imports": "off",
+    "perfectionist/sort-named-imports": "off",
+    "regexp/prefer-d": "off",
+    "regexp/no-useless-escape": "off",
+    "regexp/no-unused-capturing-group": "off",
+    "regexp/strict": "off",
+    "style/max-len": ["error", { code: 88 }],
+    "style/quotes": "error",
+    "ts/no-unused-vars": ["error", {
+      args: "all",
+      argsIgnorePattern: "^_",
+      varsIgnorePattern: "^_",
+    }],
+    "ts/no-explicit-any": "error",
+    "ts/no-unused-expressions": "off",
+    "ts/consistent-type-definitions": "off",
+    "unicorn/filename-case": ["error", {
+      case: "kebabCase",
+      ignore: ["README.md", "LICENSE"],
+    }],
+  },
+})
